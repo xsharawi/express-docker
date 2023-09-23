55a11e59a851c933af9a605f3b3a4a3e5fb61670
xsharawi <mohammadsharawi14@gmail.com>
2023-09-23 15:37:19 +0300

    check for /

--- main.ts
+++ main.ts
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
-	res.json({ msg: "Hello from express" })
+	res.json({ msg: "I LOVE DOCKERING" })
 })
 
 app.use("/users", Container.get(UsersController).router)
